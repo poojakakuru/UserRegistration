fc07d94d91f502bdc7015c727b29c65868329431
poojakakuru <poojareddykakuru@gmail.com>
2020-10-14 12:17:09 +0530

    email is validated

--- userRegistration.sh
+++ userRegistration.sh
@@ -21,3 +21,14 @@ then
 else
    echo "invalid firstname,please enter valid first name"
 fi
+
+read -p "Enter your emailID:" email
+
+emailpattern="^[A-Za-z0-9]+([._+-][0-9a-zA-Z]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?"
+
+if [[ $email =~ $emailpattern ]]
+then
+   echo "valid email"
+else
+   echo "invalid email"
+fi
