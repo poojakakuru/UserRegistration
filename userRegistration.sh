fb16421ca9d371fe6f8ebc0e3d9fc998e2a616ab
poojakakuru <poojareddykakuru@gmail.com>
2020-10-14 11:58:16 +0530

    first user name is validated

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Welcome to user registration program"
+
+read -p "Enter valid user name :" username
+
+usernamepattern="^[[:upper:]][[:lower:]]{3,}"
+
+if [[ $username =~ $usernamepattern ]]
+then
+   echo "valid username"
+else
+   echo "invalid username"
+fi
