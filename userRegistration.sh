1e7dd495312b8277fb44d60fec16f9e527cba5cc
poojakakuru <poojareddykakuru@gmail.com>
2020-10-14 12:07:02 +0530

    validated last name is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,13 +2,22 @@
 
 echo "Welcome to user registration program"
 
-read -p "Enter valid user name :" username
+read -p "Enter valid first name :" firstname
 
-usernamepattern="^[[:upper:]][[:lower:]]{3,}"
+usernamepattern="^[[:upper:]][[:lower:]]{2,}"
 
-if [[ $username =~ $usernamepattern ]]
+if [[ $firstname =~ $usernamepattern ]]
 then
-   echo "valid username"
+   echo "valid firstname"
+   
+   read -p "Enter valid last name:" lastname
+
+   if [[ $lastname =~ $usernamepattern ]]
+   then
+      echo "Valid lastname"
+   else
+      echo "Invalid lastname,please enter valid last name"
+   fi
 else
-   echo "invalid username"
+   echo "invalid firstname,please enter valid first name"
 fi
